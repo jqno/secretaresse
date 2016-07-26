4bc754c60840c37d7a437626dd16130ea15492f3
Jan Ouwens <jan.ouwens@gmail.com>
2016-07-26 17:07:25 +0200

    Working proof-of-concept

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "exchange"
+organization := "nl.jqno.exchange"
+version := "0.0.1-SNAPSHOT"
+
+scalaVersion := "2.11.8"
+scalacOptions += "-deprecation"
+
+libraryDependencies ++= Seq(
+  "com.microsoft.ews-java-api" % "ews-java-api" % "2.0",
+  "com.google.api-client" % "google-api-client" % "1.22.0",
+  "com.google.oauth-client" % "google-oauth-client-jetty" % "1.22.0",
+  "com.google.apis" % "google-api-services-calendar" % "v3-rev202-1.22.0",
+  "com.typesafe" % "config" % "1.3.0"
+)
+
---
+++ src/main/scala/nl/jqno/secretaresse/secretaresse.scala
@@ -0,0 +1,187 @@
+package nl.jqno.secretaresse
+
+import java.io.{File, FileReader}
+import java.net.URI
+import java.util.{Date, GregorianCalendar}
+
+import scala.collection.JavaConverters._
+
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
+import com.google.api.client.googleapis.auth.oauth2.{GoogleAuthorizationCodeFlow, GoogleClientSecrets}
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.util.DateTime
+import com.google.api.client.util.store.FileDataStoreFactory
+import com.google.api.services.calendar.{Calendar, CalendarScopes}
+import com.google.api.services.calendar.model.{Event, EventDateTime}
+import com.typesafe.config.ConfigFactory
+import microsoft.exchange.webservices.data.core.{ExchangeService, PropertySet}
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion
+import microsoft.exchange.webservices.data.core.enumeration.property.{BasePropertySet, BodyType, WellKnownFolderName}
+import microsoft.exchange.webservices.data.core.service.folder.CalendarFolder
+import microsoft.exchange.webservices.data.credential.WebCredentials
+import microsoft.exchange.webservices.data.search.CalendarView
+import com.typesafe.config.Config
+
+case class Appointment(startDate: Date, endDate: Date, subject: String, location: String, body: String, googleId: Option[String] = None) {
+
+  override def toString = s"($startDate - $endDate) @ $location -> $subject"
+
+  override def equals(obj: Any) = obj match {
+    case other: Appointment =>
+      startDate == other.startDate && endDate == other.endDate && subject == other.subject && location == other.location && body == other.body
+    case _ =>
+      false
+  }
+
+  override def hashCode = {
+    val prime = 59
+    var result = prime
+    result = prime * result + startDate.##
+    result = prime * result + endDate.##
+    result = prime * result + subject.##
+    result = prime * result + location.##
+    result = prime * result + body.##
+    result
+  }
+}
+
+object Secretaresse extends App {
+
+  def loadConfig: Config = {
+    // TODO: ceedubs ficus
+    val location = args.headOption getOrElse "application.conf"
+    println(s"Loading $location")
+    val externalConfig = ConfigFactory.parseFile(new File(location))
+    ConfigFactory.load(externalConfig)
+  }
+
+  def window(daysPast: Int, daysFuture: Int): (Date, Date) = {
+    val now = new GregorianCalendar()
+    val start = now.clone().asInstanceOf[GregorianCalendar]
+    start.add(java.util.Calendar.DATE, -daysPast)
+    val end = now.clone().asInstanceOf[GregorianCalendar]
+    end.add(java.util.Calendar.DATE, daysFuture)
+    (start.getTime, end.getTime)
+  }
+
+  def getAppointmentsFromExchange(config: Config, from: Date, to: Date): Set[Appointment] = {
+    val service = new ExchangeService(ExchangeVersion.Exchange2010_SP2)
+    val credentials = new WebCredentials(config.getString("exchange.userName"), config.getString("exchange.password"))
+    service.setCredentials(credentials)
+    service.setUrl(new URI(config.getString("exchange.url")))
+    val propertySet = new PropertySet(BasePropertySet.FirstClassProperties)
+    propertySet.setRequestedBodyType(BodyType.Text)
+    val cf = CalendarFolder.bind(service, WellKnownFolderName.Calendar)
+    val results = cf.findAppointments(new CalendarView(from, to)).getItems.asScala
+
+    val appointments = results map { appt =>
+      appt.load(propertySet)
+      val subject = Option(appt.getSubject) getOrElse ""
+      val location = Option(appt.getLocation) getOrElse ""
+      val body = Option(appt.getBody.toString) getOrElse ""
+      Appointment(appt.getStart, appt.getEnd, subject, location, body)
+    }
+    appointments.toSet
+  }
+
+  def buildGoogleCalendarService(config: Config): Calendar = {
+    // global initializations
+    val jsonFactory = JacksonFactory.getDefaultInstance
+    val httpTransport = GoogleNetHttpTransport.newTrustedTransport
+    val scopes = List(CalendarScopes.CALENDAR).asJava
+    val dataStoreDir = new File(config.getString("google.dataStoreDir"))
+    val dataStoreFactory = new FileDataStoreFactory(dataStoreDir)
+
+    // load secrets
+    val in = new FileReader(config.getString("google.secrets"))
+    val secrets = GoogleClientSecrets.load(jsonFactory, in)
+
+    // build flow and trigger authorization request
+    val flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory, secrets, scopes)
+        .setDataStoreFactory(dataStoreFactory)
+        .setAccessType("offline")
+        .build
+    val credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver).authorize("user")
+
+    // build an authorized Google Calendar service
+    new Calendar.Builder(httpTransport, jsonFactory, credential)
+        .setApplicationName("Secretaresse")
+        .build
+  }
+
+  def getCalendarId(service: Calendar, name: String): String = {
+    val calendars = service.calendarList.list.execute().getItems.asScala
+    calendars.find(_.getSummary == name).get.getId
+  }
+
+  def getAppointmentsFromGoogle(service: Calendar, calendarId: String, from: Date, to: Date): Set[Appointment] = {
+    // list the next 10 items from the specified calendar
+    val events = service.events.list(calendarId)
+        .setTimeMin(new DateTime(from))
+        .setTimeMax(new DateTime(to))
+        .setOrderBy("startTime")
+        .setSingleEvents(true)
+        .execute()
+        .getItems.asScala
+
+    val appointments = events map { event =>
+      val start = Option(event.getStart.getDateTime) getOrElse event.getStart.getDate
+      val end = Option(event.getEnd.getDateTime) getOrElse event.getEnd.getDate
+      val summary = Option(event.getSummary) getOrElse ""
+      val location = Option(event.getLocation) getOrElse ""
+      val description = Option(event.getDescription) getOrElse ""
+      Appointment(new Date(start.getValue), new Date(end.getValue), summary, location, description, Some(event.getId))
+    }
+    appointments.toSet
+  }
+
+  def removeAppointmentsFromGoogle(service: Calendar, calendarId: String, toRemove: Set[Appointment]): Unit = {
+    toRemove foreach { appt => appt.googleId match {
+      case Some(id) => service.events.delete(calendarId, id).execute()
+      case _ => // do nothing
+    }}
+  }
+
+  def addAppointmentsToGoogle(service: Calendar, calendarId: String, toAdd: Set[Appointment]): Unit = {
+    val events = toAdd map { appt =>
+      new Event()
+        // TODO: use .setDate for All-day events
+        .setStart(new EventDateTime().setDateTime(new DateTime(appt.startDate)))
+        .setEnd(new EventDateTime().setDateTime(new DateTime(appt.endDate)))
+        .setSummary(appt.subject)
+        .setLocation(appt.location)
+        .setDescription(appt.body)
+    }
+    events foreach { e =>
+      service.events.insert(calendarId, e).execute()
+    }
+  }
+
+  def toRemove(source: Set[Appointment], target: Set[Appointment]): Set[Appointment] = target -- source
+  def toAdd(source: Set[Appointment], target: Set[Appointment]): Set[Appointment] = source -- target
+
+
+  val config = loadConfig
+
+  val (startDate, endDate) = window(config.getInt("app.pastDays"), config.getInt("app.futureDays"))
+
+  println("Getting events from Exchange...")
+  val exchange = getAppointmentsFromExchange(config, startDate, endDate)
+  println("Connecting to Google...")
+  val calendarService = buildGoogleCalendarService(config)
+  val calendarId = getCalendarId(calendarService, config.getString("google.calendarName"))
+  println("Getting events from Google...")
+  val google = getAppointmentsFromGoogle(calendarService, calendarId, startDate, endDate)
+
+  val itemsToRemove = toRemove(exchange, google)
+  println(s"Removing ${itemsToRemove.size} events from Google...")
+  itemsToRemove foreach println
+  removeAppointmentsFromGoogle(calendarService, calendarId, itemsToRemove)
+
+  val itemsToAdd = toAdd(exchange, google)
+  println(s"Adding ${itemsToAdd.size} events to Google...")
+  itemsToAdd foreach println
+  addAppointmentsToGoogle(calendarService, calendarId, itemsToAdd)
+}
