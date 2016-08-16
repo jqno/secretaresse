215b7f77d20b00a696258fb27596901a9aaae99f
Jan Ouwens <jan.ouwens@gmail.com>
2016-08-16 07:46:27 +0200

    Moves Appointment to its own file

--- src/main/scala/nl/jqno/secretaresse/Secretaresse.scala
+++ src/main/scala/nl/jqno/secretaresse/Secretaresse.scala
@@ -24,31 +24,6 @@ import microsoft.exchange.webservices.data.core.service.folder.CalendarFolder
 import microsoft.exchange.webservices.data.credential.WebCredentials
 import microsoft.exchange.webservices.data.search.CalendarView
 
-case class Appointment(
-    startDate: Date, endDate: Date, subject: String, location: String,
-    body: String, isAllDay: Boolean, googleId: Option[String] = None) {
-
-  override def toString = s"${if (googleId.isDefined) "GOOGLE" else "EXCHNG"}  ${if (isAllDay) "A" else " "}($startDate - $endDate) @ $location -> $subject"
-
-  override def equals(obj: Any) = obj match {
-    case other: Appointment =>
-      startDate == other.startDate && endDate == other.endDate && subject == other.subject && location == other.location && body == other.body
-    case _ =>
-      false
-  }
-
-  override def hashCode = {
-    val prime = 59
-    var result = prime
-    result = prime * result + startDate.##
-    result = prime * result + endDate.##
-    result = prime * result + subject.##
-    result = prime * result + location.##
-    result = prime * result + body.##
-    result
-  }
-}
-
 object Secretaresse extends App {
 
   def loadConfig: Config = {
