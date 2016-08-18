cd93b0194442647887ec0f87d98aa7d95eea8089
Jan Ouwens <jan.ouwens@gmail.com>
2016-08-18 16:59:40 +0200

    Allows for the non-thread-safety of Exchange API

--- src/main/scala/nl/jqno/secretaresse/ExchangeInterface.scala
+++ src/main/scala/nl/jqno/secretaresse/ExchangeInterface.scala
@@ -11,15 +11,14 @@ import microsoft.exchange.webservices.data.core.service.item.{Appointment => Exc
 import microsoft.exchange.webservices.data.core.{ExchangeService, PropertySet}
 import microsoft.exchange.webservices.data.credential.WebCredentials
 import microsoft.exchange.webservices.data.search.CalendarView
-import nl.jqno.secretaresse.Util.async
 
 import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{Future, blocking}
 
 class ExchangeInterface(config: Config) {
 
-  def getAppointments(from: Date, to: Date): Future[Set[Appointment]] = {
+  def getAppointments(from: Date, to: Date): Future[Set[Appointment]] = Future {
     val service = new ExchangeService(ExchangeVersion.Exchange2010_SP2)
     val credentials = new WebCredentials(config.getString("exchange.userName"), config.getString("exchange.password"))
     service.setCredentials(credentials)
@@ -27,15 +26,13 @@ class ExchangeInterface(config: Config) {
     val propertySet = new PropertySet(BasePropertySet.FirstClassProperties)
     propertySet.setRequestedBodyType(BodyType.Text)
 
-    for {
-      cf <- async { CalendarFolder.bind(service, WellKnownFolderName.Calendar) }
-      appointments <- async { cf.findAppointments(new CalendarView(from, to)).getItems.asScala.toSet }
-      result <- Future.sequence(appointments.map(convert(propertySet, _)))
-    } yield result
+    val cf = blocking { CalendarFolder.bind(service, WellKnownFolderName.Calendar) }
+    val appointments = blocking { cf.findAppointments(new CalendarView(from, to)).getItems.asScala.toSet }
+    appointments.map(convert(propertySet, _))
   }
 
-  private def convert(propertySet: PropertySet, appt: ExchangeAppointment): Future[Appointment] = async {
-    appt.load(propertySet)
+  private def convert(propertySet: PropertySet, appt: ExchangeAppointment): Appointment = {
+    blocking { appt.load(propertySet) }
     val subject = Option(appt.getSubject) getOrElse ""
     val location = Option(appt.getLocation) getOrElse ""
     val body = Option(appt.getBody.toString) getOrElse ""
