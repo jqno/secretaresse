ffe97e796e37f2045464d53acc9d081a7171b5d3
Teije van Sloten <teije.van.sloten@hva.nl>
2016-08-19 15:55:59 +0200

    Added Tray class

--- src/main/scala/nl/jqno/secretaresse/Main.scala
+++ src/main/scala/nl/jqno/secretaresse/Main.scala
@@ -1,8 +1,18 @@
 package nl.jqno.secretaresse
 
+import java.awt.Toolkit.getDefaultToolkit
+import java.net.URL
+
 object Main extends App {
 
   val secretaresse = new Secretaresse(args.headOption getOrElse "application.conf")
-  secretaresse.sync()
 
+  // To make the docIcon disappear http://stackoverflow.com/questions/5057639/systemtray-based-application-without-window-on-mac-os-x
+
+  Tray().createTray(
+    tooltip = "Secretaresse app",
+    icon = getDefaultToolkit.getImage(new URL("https://github.com/encharm/Font-Awesome-SVG-PNG/blob/master/white/png/64/calendar-check-o.png?raw=true")),
+    actions = Map(
+      ("Run now", e => secretaresse.sync())
+    ))
 }
---
+++ src/main/scala/nl/jqno/secretaresse/Tray.scala
@@ -0,0 +1,33 @@
+package nl.jqno.secretaresse
+
+import java.awt._
+import java.awt.event.{ActionEvent, ActionListener}
+
+class Tray() {
+
+  def createTray(tooltip: String, icon: Image, actions: Map[String, ActionEvent => Unit]): Unit = {
+    val popup: PopupMenu = new PopupMenu()
+    actions.foreach { case (title, action) => popup.add(createMenuItem(title, action))}
+    popup.addSeparator()
+    popup.add(createMenuItem("Quit", e => System.exit(0)))
+
+    val trayIcon: TrayIcon = new TrayIcon(icon, tooltip, popup)
+    trayIcon.setImageAutoSize(true)
+
+    val tray: SystemTray = SystemTray.getSystemTray
+    tray.add(trayIcon)
+  }
+
+
+  private def createMenuItem(title: String, action: ActionEvent => Unit): MenuItem = {
+    val menuItem = new MenuItem(title)
+    menuItem.addActionListener(new ActionListener {
+      override def actionPerformed(e: ActionEvent): Unit = action(e)
+    })
+    menuItem
+  }
+}
+
+object Tray {
+  def apply() = new Tray()
+}
