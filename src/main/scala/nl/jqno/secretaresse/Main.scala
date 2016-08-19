c7a0fe7d3a5d4e5e49131c4ae024120611b0dfa1
Teije van Sloten <teije.van.sloten@hva.nl>
2016-08-19 16:41:08 +0200

    Moved timer function to a scheduler class

--- src/main/scala/nl/jqno/secretaresse/Main.scala
+++ src/main/scala/nl/jqno/secretaresse/Main.scala
@@ -2,7 +2,6 @@ package nl.jqno.secretaresse
 
 import java.awt.Toolkit.getDefaultToolkit
 import java.net.URL
-import java.util.{Timer, TimerTask}
 
 import scala.collection.immutable.ListMap
 
@@ -12,7 +11,7 @@ object Main extends App {
 
   val secretaresse = new Secretaresse(args.headOption getOrElse "application.conf")
 
-  var timer = new Timer()
+  val scheduler = Scheduler(Unit => secretaresse.sync())
 
   Tray().createTray(
     tooltip = "Secretaresse app",
@@ -21,27 +20,14 @@ object Main extends App {
 
     actions = ListMap(
       ("Run now", e => secretaresse.sync()),
-      ("Run every 5 minutes", e => scheduleTask(5 * 60 * 1000)),
-      ("Run every 30 minutes", e => scheduleTask(30 * 60 * 1000)),
-      ("Run every hour", e => scheduleTask(60 * 60 * 1000)),
-      ("Stop running", e => stopTimer())
+      ("Run every 5 minutes", e => scheduler.schedule(5 * 60 * 1000)),
+      ("Run every 30 minutes", e => scheduler.schedule(30 * 60 * 1000)),
+      ("Run every hour", e => scheduler.schedule(60 * 60 * 1000)),
+      ("Stop running", e => scheduler.stop())
     ))
 
-  def scheduleTask(period: Int) = {
-    stopTimer()
-    timer.schedule(new TimerTask {
-      override def run(): Unit = secretaresse.sync()
-    }, 1000, period)
-  }
-
-  def stopTimer() = {
-    timer.cancel()
-    timer.purge()
-    timer = new Timer()
-  }
 
   while (true)
     Thread.sleep(30000)
 
-  timer.cancel()
 }
